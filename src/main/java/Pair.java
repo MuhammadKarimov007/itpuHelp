b91363c35ca0afad24fd1d588a9422105dd35591
Muhammad Karimov <121921870+MuhammadKarimov007@users.noreply.github.com>
2024-07-06 20:48:59 +0500

    changed var names

--- src/main/java/Pair.java
+++ src/main/java/Pair.java
@@ -1,16 +1,16 @@
 import java.util.Objects;
 
-public record Pair(int a, int b) {
+public record Pair(int val1, int val2) {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Pair pair = (Pair) o;
-        return Math.abs(a - b) == Math.abs(pair.a - pair.b);
+        return Math.abs(val1 - val2) == Math.abs(pair.val1 - pair.val2);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(Math.abs(a - b));
+        return Objects.hash(Math.abs(val1 - val2));
     }
 }
