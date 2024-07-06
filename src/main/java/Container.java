1cb6d3c5a4c9ad6ed5fcf9fd29de33e7da49a29c
Muhammad Karimov <shermuhammad.karimov2004@mail.ru>
2024-07-06 16:13:22 +0500

    implemented methods according to instructions

--- src/main/java/Container.java
+++ src/main/java/Container.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Container {
+import java.util.*;
+
+
+public class Container {
+    private Set<Pair> pairs;
+
+    public Container() {
+    }
+
+    public Container(List<Pair> list) {
+        this.pairs = new HashSet<>(list);
+    }
+
+    public List<String> process() {
+        List<String> processedNums = new ArrayList<>();
+
+        for (Pair pair : pairs) {
+            int pairSum = pair.a() + pair.b();
+            if ( pairSum > 0 ) {
+                processedNums.add(Integer.toString(pairSum));
+            }
+        }
+
+        processedNums.sort(Comparator.reverseOrder());
+
+        return processedNums;
+    }
+
+    public void removeOnCondition() {
+        pairs.removeIf(tempPair -> tempPair.a() * tempPair.b() >= 0);
+    }
 }
--- src/main/java/Pair.java
+++ src/main/java/Pair.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public record Pair() {
+import java.util.Objects;
+
+public record Pair(int a, int b) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair pair = (Pair) o;
+        return Math.abs(a - b) == Math.abs(pair.a - pair.b);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Math.abs(a - b));
+    }
 }
